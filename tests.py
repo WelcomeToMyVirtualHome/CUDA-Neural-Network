2d5e2572f97bbd35c58973fa788c8f1844c933a6
Paweł <paffcio314@gmail.com>
2019-06-09 15:38:09 +0200

    Some cprofiler tests added

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@ from nn import *
 from data import *
 from layer import *
 
-def init_nn():
-	nn = NeuralNetwork()
-	nn.learning_rate = 0.001
+def init_nn(cuda = False):
+	nn = NeuralNetwork(cuda)
+	nn.learning_rate = 0.01
 	nn.addLayer(LinearLayer("Linear1",2,10))
 	nn.addLayer(ReLULayer("ReLU1"))
 	nn.addLayer(LinearLayer("Linear2",10,6))
@@ -20,18 +20,21 @@ def init_nn():
 	return nn
 
 def train_test(filename, nn, epochs, data, log = False, cuda = False):
-	cProfile.run('nn.train(data, epochs = epochs, log = True)', filename = filename)
+	cProfile.run('nn.train(data, epochs = epochs, log = True)', filename = filename )
 
 
-nn = init_nn()
-epochs = 100
-n_points = 200
+# nn = init_nn()
+epochs = 20
 n_batches = 50
-data = [prepare_data(n_points) for i in range(0, n_batches)]
-
-filename = 'stats.txt'
-train_test(filename, nn, epochs, data, log = False, cuda = False)
+n_points_array = [10, 20, 100, 200, 500, 1000, 2000, 5000, 10000]
+filename0 = 'gpu_stats'
+for n in n_points_array:
+	filename = filename0 + str(n)
+	nn = init_nn(True)
+	data = [prepare_data(n) for i in range(0, n_batches)]
+	train_test(filename, nn, epochs, data, log = False, cuda = False)
 
 
 p = pstats.Stats(filename)
-p.sort_stats('cumulative').print_stats(10)
+p.strip_dirs().sort_stats('cumulative').print_stats(10)
+p.strip_dirs().sort_stats('time').print_stats(10)
